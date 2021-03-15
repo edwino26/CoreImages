5e79f6cff2cbf8c739b1c2f9102cdcd21b1f0235
edwino26 <ortega.edwin.y@gmail.com>
2021-03-15 16:42:38 -0500

    Commit File Structure Changes and Start Main ML File

---
+++ .history/ML_EO_20210315163350.py
@@ -0,0 +1,30 @@
+# # lectura de los xlsx del procesamiento de imagenes 
+# y la informacion obtenida de los registros por pozo
+# %%
+import numpy as np
+import pandas as pd
+import os
+import os.path
+import matplotlib.pyplot as plt  # GRAPHS
+import glob
+import seaborn as sns
+
+
+#%%
+T2 = pd.read_excel('./Excel_Files/T2.xls',sheet_name='T2_data')
+Img_T2 = pd.read_excel('./Excel_Files/Processed_Images_T2.xls',sheet_name='T2')
+T6 = pd.read_excel('./Excel_Files/T6.xls',sheet_name='T6_data')
+Img_T6 = pd.read_excel('./Excel_Files/Processed_Images_T6.xls',sheet_name='T6')
+
+# u18=pd.read_excel('./Excel_Files/U18.xls',sheet_name='U18_data')
+# df= pd.DataFrame()
+
+df = T2.append(T6)
+
+print(df)
+sns.heatmap(df.drop(['WELL', 'DEPTH'), axis=1))
+
+# %%
+
+
+    
--- images T6 .py
+++ images T6 .py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+from scipy import interpolate
 
 
 
@@ -124,11 +125,22 @@ for k in range(len(uvFiles)): #Loop through various files containing images
 
 # %%
 
+
 d = {'DEPTH': DEPTH, 'GRAY': GRAY}
 sub = pd.DataFrame(np.array(DEPTH).T, columns = ['DEPTH'])
 sub['GRAY'] = np.array(GRAY).T
 sub['PHOTO'] = np.array(PHOTO).T
-sub.to_excel("Processed_Images_T6.xlsx",sheet_name=Well) 
+
+
+dep= np.arange(doo,dnn,0.25)
+sub_S = pd.DataFrame(dep.T, columns = ['DEPTH'])
+sub_S['DEPTH'] = dep
+f = interpolate.interp1d(sub['DEPTH'], sub['GRAY'])
+sub_S['GRAY'] = f(dep)
+p = interpolate.interp1d(sub['DEPTH'], sub['PHOTO'])
+sub_S['PHOTO'] = p(dep)
+
+sub_S.to_excel("./Excel_Files/Processed_Images_T6.xls",sheet_name=Well) 
  
 # %%
 dplot_o = 3377
